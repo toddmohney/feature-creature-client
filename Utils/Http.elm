c1f8766fda5e55aab856525251fc7d16308917ac
Todd Mohney <toddmohney@gmail.com>
2015-11-23 14:44:54 -0500

    [#40] Remove unused request headers

--- Utils/Http.elm
+++ Utils/Http.elm
@@ -5,10 +5,7 @@ import Http
 jsonPostRequest : String -> String -> Http.Request
 jsonPostRequest url jsonEncodedRequestBody =
   { verb = "POST"
-  , headers = [ ("Origin", "http://localhost:8000")
-              , ("Access-Control-Request-Method", "POST")
-              , ("Content-Type", "application/json")
-              ]
+  , headers = [ ("Content-Type", "application/json") ]
   , url = url
   , body = Http.string jsonEncodedRequestBody
   }
