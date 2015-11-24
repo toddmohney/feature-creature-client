7a82d3638795edc2228f8746cfeaf184e36f0849
Todd Mohney <toddmohney@gmail.com>
2015-11-23 22:24:19 -0500

    [#46] Move product list into Product Page main nav
    Resolves gust/feature-creature#46

--- UI/App/Components/ListDetailView.elm
+++ UI/App/Components/ListDetailView.elm
@@ -8,7 +8,7 @@ import Html.Attributes exposing (class)
 listDetailView : List Html -> List Html -> Html
 listDetailView listHtml detailHtml =
   Html.div
-    []
+    [ class "clearfix" ]
     [ Html.div [ class "pull-left" ] listHtml
     , Html.div [ class "pull-right" ] detailHtml
     ]
--- UI/App/Components/ProductPageNavBar.elm
+++ UI/App/Components/ProductPageNavBar.elm
@@ -1,64 +1,96 @@
 module UI.App.Components.ProductPageNavBar where
 
-import Html exposing (Html, Attribute)
+import Effects exposing (Effects)
+import Html exposing (Html)
 import Html.Attributes exposing (attribute, class, classList, href, id)
+import Html.Events exposing (onClick)
+import Products.Product exposing (Product)
+import UI.App.Components.NavBar as NavBar exposing (NavBarItem)
 
-type alias NavBarItem =
-  { attributes : List Attribute
-  , html       : Html
+type alias ProductPageNavBar =
+  { products        : List Product
+  , selectedProduct : Product
+  , selectedView    : ProductViewOption
   }
 
-renderNavBar : List NavBarItem -> Html
-renderNavBar navBarItems =
-  Html.nav
-    [ classList [("navbar", True), ("navbar-inverse", True)] ]
-    [ Html.div
-        [ class "container-fluid" ]
-        [ navBarHeader
-        , renderItems navBarItems
+type Action = SelectFeaturesView
+            | SelectDomainTermsView
+            | SetSelectedProduct Product
+
+type ProductViewOption = FeaturesViewOption
+                       | DomainTermsViewOption
+
+init : List Product -> Product -> ProductPageNavBar
+init products selectedProduct =
+  { products = products
+  , selectedProduct = selectedProduct
+  , selectedView = FeaturesViewOption
+  }
+
+update : Action -> ProductPageNavBar -> (ProductPageNavBar, Effects Action)
+update action navBar =
+  case action of
+    SelectFeaturesView ->
+      ( { navBar | selectedView <- FeaturesViewOption }
+      , Effects.none
+      )
+
+    SelectDomainTermsView ->
+      ( { navBar | selectedView <- DomainTermsViewOption }
+      , Effects.none
+      )
+
+    SetSelectedProduct product ->
+      ( { navBar | selectedProduct <- product }
+      , Effects.none
+      )
+
+view : Signal.Address Action -> ProductPageNavBar -> Html
+view address navBar =
+  NavBar.renderNavBar
+    [ renderProductsDropdownItem address navBar
+    , renderFeaturesItem address navBar
+    , renderDomainTermsItem address navBar
+    ]
+
+renderProductsDropdownItem : Signal.Address Action -> ProductPageNavBar -> NavBarItem
+renderProductsDropdownItem address navBar =
+  { attributes = [ class "dropdown" ]
+  , html =
+    [ Html.a
+        [ class "dropdown-toggle"
+        , attribute "data-toggle" "dropdown"
+        , attribute "role" "button"
+        ]
+        [ Html.text navBar.selectedProduct.name
+        , Html.span [ class "caret" ] []
+        ]
+    , Html.ul [ class "dropdown-menu" ]
+        <| List.map (renderProductItem address) navBar.products ++
+        [ Html.li [ class "divider", attribute "role" "separator" ] []
+        , Html.li [] [ Html.a [ href "#" ] [ Html.text "Create New Product" ] ]
         ]
     ]
+  }
 
-renderItems : List NavBarItem -> Html
-renderItems items =
-  Html.div
-    [ classList [("collapse", True), ("navbar-collapse", True)]
-    , id "main_nav"
-    ]
-    [ Html.ul
-        [ classList [("nav", True), ("navbar-nav", True)] ]
-        (List.map renderItem items)
-    ]
+renderFeaturesItem : Signal.Address Action -> ProductPageNavBar -> NavBarItem
+renderFeaturesItem address navBar =
+  { attributes = [ classList [("active", navBar.selectedView == FeaturesViewOption)] ]
+  , html = [ featuresLink address ]
+  }
 
-renderItem : NavBarItem -> Html
-renderItem item =
-  Html.li item.attributes [item.html]
+renderDomainTermsItem : Signal.Address Action -> ProductPageNavBar -> NavBarItem
+renderDomainTermsItem address navBar =
+  { attributes = [ classList [("active", navBar.selectedView == DomainTermsViewOption)] ]
+  , html = [ domainTermsLink address ]
+  }
 
-navBarHeader : Html
-navBarHeader =
-  Html.div
-    [ class "navbar-header" ]
-    [ navBarCollapsibleNavButton
-    , navBarBrandButton
-    ]
+renderProductItem : Signal.Address Action -> Product -> Html
+renderProductItem address product =
+  Html.li [] [ Html.a [ onClick address (SetSelectedProduct product) ] [ Html.text product.name ] ]
 
-navBarBrandButton : Html
-navBarBrandButton =
-  Html.a
-    [ class "navbar-brand", href "#" ]
-    [ Html.text "FeatureCreature" ]
+featuresLink : Signal.Address Action -> Html
+featuresLink address = Html.a [ onClick address SelectFeaturesView ] [ Html.text "Features" ]
 
-navBarCollapsibleNavButton : Html
-navBarCollapsibleNavButton =
-  Html.button
-    [ attribute "type" "button"
-    , attribute "data-toggle" "collapse"
-    , attribute "data-target" "#main_nav"
-    , attribute "aria-expanded" "false"
-    , classList [("navbar-toggle", True), ("collapsed", True)]
-    ]
-    [ Html.span [ class "icon-bar" ] []
-    , Html.span [ class "icon-bar" ] []
-    , Html.span [ class "icon-bar" ] []
-    ]
-
+domainTermsLink : Signal.Address Action -> Html
+domainTermsLink address = Html.a [ onClick address SelectDomainTermsView ] [ Html.text "DomainTerms" ]
