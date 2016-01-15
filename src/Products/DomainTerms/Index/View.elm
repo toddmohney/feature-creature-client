2b97d6fedb1fbdedea23b0aa8a766b803a8214c7
Todd Mohney <toddmohney@gmail.com>
2016-01-15 16:25:53 -0500

    Clarify which actions are being referenced
    [gust/feature-creature#55]
    [gust/feature-creature#56]

--- src/Products/DomainTerms/Index/View.elm
+++ src/Products/DomainTerms/Index/View.elm
@@ -6,20 +6,20 @@ import Html                                          exposing (Html)
 import Html.Events                                   exposing (onClick)
 import Html.Attributes                               exposing (class, href)
 import Products.DomainTerms.DomainTerm               exposing (DomainTerm, toSearchQuery)
-import Products.DomainTerms.Index.Actions as Actions exposing (Action(..))
+import Products.DomainTerms.Index.Actions as Actions exposing (DomainTermAction(..))
 import Products.DomainTerms.Index.ViewModel          exposing (DomainTermsView)
 import Products.DomainTerms.Forms.View    as DTF
 import UI.App.Components.Panels           as UI
 
 
-view : Signal.Address Action -> DomainTermsView -> Html
+view : Signal.Address DomainTermAction -> DomainTermsView -> Html
 view address domainTermsView =
   let domainTerms       = domainTermsView.product.domainTerms
       forwardedAddress  = Signal.forwardTo address Actions.DomainTermFormAction
       newDomainTermForm = DTF.view forwardedAddress domainTermsView.domainTermForm
   in Html.div [] (newDomainTermForm :: (List.map (renderDomainTerm address) domainTerms))
 
-renderDomainTerm : Signal.Address Action -> DomainTerm -> Html
+renderDomainTerm : Signal.Address DomainTermAction -> DomainTerm -> Html
 renderDomainTerm address domainTerm =
   let domainTermName = Html.div [ class "pull-left" ] [ Html.text domainTerm.title ]
       linkAction     = SearchFeatures (toSearchQuery domainTerm)
