2f73a12434d145fffbfd70a8bf427c8d3c121b5c
Todd Mohney <toddmohney@gmail.com>
2016-02-19 21:55:38 -0500

    Refactor forms module

--- src/UI/App/Primitives/Forms.elm
+++ src/UI/App/Primitives/Forms.elm
@@ -24,93 +24,41 @@ input : Signal.Address a -> InputField a -> Html
 input address { defaultValue, inputName, inputParser, labelContent, validationErrors } =
   let hasErrors = not <| List.isEmpty validationErrors
       errorMsgs = List.map Html.text validationErrors
-      errorHelpText =
-        Html.span
-          [ classList [("help-block", True)] ]
-          errorMsgs
       formGroup =
-        Html.div
-          [ classList
-              [ ("form-group", True)
-              , ("has-error", hasErrors)
-              , ("has-feedback", hasErrors)
-              ]
+        formGroupContainer
+          [ Html.label [ for inputName ] [ labelContent ]
+          , Html.input [ class "form-control", id inputName, name inputName, value defaultValue, onInput address inputParser ] []
+          , inputErrorIcon hasErrors
           ]
-          [ Html.label
-              [ for inputName ]
-              [ labelContent ]
-          , Html.input
-              [ class "form-control"
-              , id inputName
-              , name inputName
-              , value defaultValue
-              , onInput address inputParser
-              ]
-              []
-          , Html.span
-              [ classList
-                  [ ("glyphicon", hasErrors)
-                  , ("glyphicon-remove", hasErrors)
-                  , ("form-control-feedback", hasErrors)
-                  ]
-              ]
-              []
-          ]
+          hasErrors
   in
-    Html.div [] [ formGroup, errorHelpText ]
+    Html.div [] [ formGroup, (errorHelpText errorMsgs) ]
 
 textarea : Signal.Address a -> InputField a -> Html
 textarea address { defaultValue, inputName, inputParser, labelContent, validationErrors } =
   let hasErrors = not <| List.isEmpty validationErrors
       errorMsgs = List.map Html.text validationErrors
-      errorHelpText =
-        Html.span
-          [ classList [("help-block", True)] ]
-          errorMsgs
       formGroup =
-        Html.div
-          [ classList
-              [ ("form-group", True)
-              , ("has-error", hasErrors)
-              , ("has-feedback", hasErrors)
-              ]
+        formGroupContainer
+          [ Html.label [ for inputName ] [ labelContent ]
+          , Html.textarea [ class "form-control", id inputName, name inputName, value defaultValue, onInput address inputParser ] []
+          , inputErrorIcon hasErrors
           ]
-          [ Html.label
-              [ for inputName ]
-              [ labelContent ]
-          , Html.textarea
-              [ class "form-control"
-              , id inputName
-              , name inputName
-              , value defaultValue
-              , onInput address inputParser
-              ]
-              []
-          , Html.span
-              [ classList
-                  [ ("glyphicon", hasErrors)
-                  , ("glyphicon-remove", hasErrors)
-                  , ("form-control-feedback", hasErrors)
-                  ]
-              ]
-              []
-          ]
+          hasErrors
   in
-     Html.div [] [ formGroup, errorHelpText ]
+    Html.div [] [ formGroup, (errorHelpText errorMsgs) ]
 
 cancelButton : Attribute -> Html
 cancelButton cancelAction =
   let cancelBtnAttributes = cancelAction :: [ BS.btn ]
-  in Html.button
-       cancelBtnAttributes
-       [ text "Cancel" ]
+  in
+    Html.button cancelBtnAttributes [ text "Cancel" ]
 
 submitButton : Attribute -> Html
 submitButton submitAction =
   let submitBtnAttributes = submitAction :: [ BS.primaryBtn ]
-  in Html.button
-       submitBtnAttributes
-       [ text "Submit" ]
+  in
+    Html.button submitBtnAttributes [ text "Submit" ]
 
 onInput : Signal.Address a -> (String -> a) -> Attribute
 onInput address contentToValue =
@@ -118,3 +66,29 @@ onInput address contentToValue =
     "input"
     Html.Events.targetValue
     (\str -> Signal.message address (contentToValue str))
+
+inputErrorIcon : Bool -> Html
+inputErrorIcon hasErrors =
+  let inputErrorClasses = classList
+                            [ ("glyphicon", hasErrors)
+                            , ("glyphicon-remove", hasErrors)
+                            , ("form-control-feedback", hasErrors)
+                            ]
+  in
+    Html.span [ inputErrorClasses ] []
+
+errorHelpText : List Html -> Html
+errorHelpText errorMsgs =
+  Html.span
+    [ classList [("help-block", True)] ]
+    errorMsgs
+
+formGroupContainer : List Html -> Bool -> Html
+formGroupContainer content hasErrors =
+  let formGroupClasses = classList
+                           [ ("form-group", True)
+                           , ("has-error", hasErrors)
+                           , ("has-feedback", hasErrors)
+                           ]
+  in
+    Html.div [ formGroupClasses ] content
