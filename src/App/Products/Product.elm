e1931780b98db267078d01d0400b01966c089eee
Todd Mohney <toddmohney@gmail.com>
2016-04-02 14:02:24 -0400

    Add repo state properties to Product model

--- src/App/Products/Product.elm
+++ src/App/Products/Product.elm
@@ -10,22 +10,30 @@ type alias Product =
   { id          : Int
   , name        : String
   , repoUrl     : String
+  , repoState   : RepositoryState
+  , repoError   : Maybe String
   , featureList : External FeatureList
   , domainTerms : External (List DomainTerm)
   , userRoles   : External (List UserRole)
   }
 
+type RepositoryState = Unready
+                     | Ready
+                     | Error
+
 newProduct : Product
 newProduct = init "" ""
 
 init : String -> String -> Product
-init prodName prodRepoUrl = init' 0 prodName prodRepoUrl
+init prodName prodRepoUrl = init' 0 prodName prodRepoUrl Unready Nothing
 
-init' : Int -> String -> String -> Product
-init' prodID prodName prodRepoUrl =
+init' : Int -> String -> String -> RepositoryState -> Maybe String -> Product
+init' prodID prodName prodRepoUrl prodRepoState prodRepoError =
   { id          = prodID
   , name        = prodName
   , repoUrl     = prodRepoUrl
+  , repoState   = prodRepoState
+  , repoError   = prodRepoError
   , featureList = NotLoaded
   , domainTerms = NotLoaded
   , userRoles   = NotLoaded
--- src/App/Products/Requests.elm
+++ src/App/Products/Requests.elm
@@ -4,7 +4,7 @@ module App.Products.Requests
   ) where
 
 import App.AppConfig             exposing (..)
-import App.Products.Product as P exposing (Product)
+import App.Products.Product as P exposing (Product, RepositoryState (..))
 import Effects                   exposing (Effects)
 import Json.Encode
 import Json.Decode as Json       exposing ((:=))
@@ -34,17 +34,36 @@ parseProducts = parseProduct |> Json.list
 
 parseProduct : Json.Decoder Product
 parseProduct =
-  Json.object3
+  Json.object5
     P.init'
-    ("id"      := Json.int)
-    ("name"    := Json.string)
-    ("repoUrl" := Json.string)
+    ("productId"   := Json.int)
+    ("productName" := Json.string)
+    ("repoUrl"     := Json.string)
+    (("repoState"   := Json.string) `Json.andThen` decodeState)
+    ("repoError"   := nullOr Json.string)
+
+decodeState : String -> Json.Decoder RepositoryState
+decodeState state = Json.succeed (repoState state)
+
+repoState : String -> RepositoryState
+repoState state =
+  case state of
+    "Ready"   -> Ready
+    "Unready" -> Unready
+    _         -> Error
+
+nullOr : Json.Decoder a -> Json.Decoder (Maybe a)
+nullOr decoder =
+  Json.oneOf
+    [ Json.null Nothing
+    , Json.map Just decoder
+    ]
 
 encodeProduct : Product -> String
 encodeProduct product =
   Json.Encode.encode 0
     <| Json.Encode.object
-        [ ("name",    Json.Encode.string product.name)
+        [ ("productName",    Json.Encode.string product.name)
         , ("repoUrl", Json.Encode.string product.repoUrl)
         ]
 
