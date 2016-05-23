013a67a1d5c54ce5fd583e3784df5aed18f78d33
Todd Mohney <toddmohney@gmail.com>
2016-05-23 12:52:00 -0400

    Finally compiling ...now to wire everything back together

--- src/Main.elm
+++ src/Main.elm
@@ -1,30 +1,35 @@
 port module Main exposing (..)
 
 import App.App  as App
-import App.Messages as App
+-- import App.Messages as App
 import App.Update  as App
-import App.AppConfig      exposing (..)
-import Html               exposing (..)
+-- import App.AppConfig      exposing (..)
+-- import Html               exposing (..)
 import Html.App as Html
 -- import UI.SyntaxHighlighting exposing (highlightSyntaxMailbox)
-import Task               exposing (Task)
 
 
-app : Program App.App
-app = Html.program
+-- app : Program App.App
+-- app = Html.program
+  -- { init   = App.init
+  -- , update = App.update
+  -- , view   = App.view
+  -- , subscriptions = [(Sub.map App.ConfigLoaded initAppConfig)]
+  -- }
+
+main : Program Never
+main = Html.program
   { init   = App.init
   , update = App.update
   , view   = App.view
-  , subscriptions = [(Sub.map App.ConfigLoaded initAppConfig)]
+  , subscriptions = (\_ -> Sub.none)
   }
-
-main = app.html
 
 -- port tasks : Signal (Task.Task Never ())
 -- port tasks = app.tasks
 
 -- port initAppConfig : Signal AppConfig
-port initAppConfig : (AppConfig -> msg) -> Sub msg
+-- port initAppConfig : (AppConfig -> msg) -> Sub msg
 
 -- port highlightSyntaxPort : Signal (Maybe ())
 -- port highlightSyntaxPort = highlightSyntaxMailbox.signal
