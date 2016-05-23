50da9c08ce3a1298eb401145bf26c262156bb5c7
Todd Mohney <toddmohney@gmail.com>
2016-05-22 21:45:55 -0400

    Upgrade ports
    [gust/feature-creature#87]

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,11 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import App.App  as App
 import App.Messages as App
 import App.Update  as App
 import App.AppConfig      exposing (..)
 import Html               exposing (..)
+import Html.App as Html
 -- import UI.SyntaxHighlighting exposing (highlightSyntaxMailbox)
 import Task               exposing (Task)
 
@@ -14,16 +15,16 @@ app = Html.program
   { init   = App.init
   , update = App.update
   , view   = App.view
-  , subscriptions = [(Signal.map App.ConfigLoaded initAppConfig)]
+  , subscriptions = [(Sub.map App.ConfigLoaded initAppConfig)]
   }
 
-main : Program Html.Html
 main = app.html
 
-port tasks : Signal (Task.Task Never ())
-port tasks = app.tasks
+-- port tasks : Signal (Task.Task Never ())
+-- port tasks = app.tasks
 
-port initAppConfig : Signal AppConfig
+-- port initAppConfig : Signal AppConfig
+port initAppConfig : (AppConfig -> msg) -> Sub msg
 
 -- port highlightSyntaxPort : Signal (Maybe ())
 -- port highlightSyntaxPort = highlightSyntaxMailbox.signal
